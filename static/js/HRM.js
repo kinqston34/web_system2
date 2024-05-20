a0ed05355ccb502eb99eef1189f19767d5d3635b
kingston <aa3741867@gmail.com>
2024-05-20 21:42:24 +0800

    save version

--- static/js/HRM.js
+++ static/js/HRM.js
@@ -2,24 +2,22 @@
 
 // 員工基本資料
 $("#baseQuery").click(()=>{
-    
     let template = $("form.query2");
-    
+    let mode = $("#mode").val();
     let csrf = $("form.query2 input[name='csrfmiddlewaretoken']").val()
     template.text("");
     template.append(`<input type="hidden" name="csrfmiddlewaretoken" value="${csrf}">`);
     template.append(`<div>在職員工列表</div>`);
-    if ($("#mode").val() == "name"){
+    if (mode == "name"){
         template.append(`<label for="name">請輸入員工姓名:</label>
         <input type="text" name="name" />
-        <button type="submit" id="dbQuery">查詢</button>`);
-    } else if ($("#mode").val() == "id"){
+        <button type="submit" id="dbQuery">查詢</button>`);    
+    } else if (mode == "id"){
         template.append(`<label for="id">請輸入員工編號:</label>
-        <input type="text" name="id">
+        <input type="text" name="id" />
         <button type="submit" id="dbQuery">查詢</button>`);
     }
 });
 
-$("#dbQuery").on("click",()=>{
-    alert("ok");
-});
+
+
